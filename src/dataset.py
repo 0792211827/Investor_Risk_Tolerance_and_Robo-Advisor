9d700fccec0422b2296cdb623748df6f91880ed4
Sylvia Bhoke <nyamosiwesley02@gmail.com.com>
2025-02-12 22:27:42 +0300

    Notebook update

--- src/dataset.py
+++ src/dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 df = pd.read_csv('/Users/sylviabhoke/Downloads/personal_repos folder/Investor_Risk_Tolerance_and_Robo-Advisor/data/raw/Risk_data.csv')
 
 df
